20f4ea2d03a55a6e4c663118d8a709fe406ecf2b
9yelin9 <9yelin9@gmail.com>
2021-08-23 17:11:05 +0900

    init

---
+++ mom.c
@@ -0,0 +1,17 @@
+// Multi-Orbital Model
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <lapack.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+	if(argc != 1) {
+		printf("Usage : %s\n", argv[0]);
+		exit(1);
+	}
+
+	return 0;
+}
